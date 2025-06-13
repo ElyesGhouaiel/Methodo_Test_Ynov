be5c264d6680aeef882203aeeb848c07a5b459f3
Ghouaiel Elyes <elyes06700@gmail.com>
2025-06-13 15:38:03 +0200

    Fix_Repo

--- TP_Aprem/Activite_seance_1/Php/src/fidelite.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-function calculerPointsFidelite(float $montant, bool $isVip): int {
-    $points = floor($montant / 10);
-    if ($isVip) {
-        $points *= 2;
-    }
-    return $points;
-}
